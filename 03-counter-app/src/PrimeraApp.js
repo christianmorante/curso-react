aad1ab8a73463a5260232b5ef300ba4a653bb47d
Christian Morante <christianmorante@outlook.com>
2021-05-06 00:51:19 -0500

    adding 03 project, hgola mundo y component

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+// Functional Components - FC
+const PrimeraApp = () => {
+    return <h1>Hola Mundo</h1>;
+
+}
+
+export default PrimeraApp;
---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import PrimeraApp from "./PrimeraApp";
+import './index.css'
+
+
+//const saludo = <h1>Hola Mundo</h1>
+
+const divRoot = document.querySelector('#app');
+//console.log(divRoot);
+
+ReactDOM.render( <PrimeraApp />, divRoot);
