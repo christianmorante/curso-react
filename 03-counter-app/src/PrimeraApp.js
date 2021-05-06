a92c3f6e7fb6b526b0e7e8c8b58c54580e793fd1
Christian Morante <christianmorante@outlook.com>
2021-05-06 00:59:17 -0500

    Fragment

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,10 +1,24 @@
+//import React, { Fragment } from 'react'
 import React from 'react'
 
 
 // Functional Components - FC
-const PrimeraApp = () => {
-    return <h1>Hola Mundo</h1>;
+// const PrimeraApp = () => {
+//     return ( 
+//         <Fragment>
+//             <h1>Hola Mundo</h1>
+//             <p>Primeros pasos con React JS</p>
+//         </Fragment>
+//     );
+// }
 
+const PrimeraApp = () => {
+    return ( 
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Primeros pasos con React JS</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
