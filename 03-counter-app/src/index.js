3da2325a421f4d843afe4aa0e4a089cfb2982c65
Christian Morante <christianmorante@outlook.com>
2021-05-07 01:46:38 -0500

    CounterApp component

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import PrimeraApp from "./PrimeraApp";
 import './index.css'
+//import PrimeraApp from "./PrimeraApp";
+import CounterApp from "./CounterApp";
 
 
 //const saludo = <h1>Hola Mundo</h1>
 
 const divRoot = document.querySelector('#app');
-//console.log(divRoot); 
 
-
-ReactDOM.render( <PrimeraApp saludo='Hola soy Chris' />, divRoot);
+//ReactDOM.render( <PrimeraApp saludo='Hola soy Chris' />, divRoot);
+ReactDOM.render( <CounterApp value={ 555 } />, divRoot);
