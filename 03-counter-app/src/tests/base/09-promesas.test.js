fc79b1adc7c52aef042c97fc2e0c38750b4eaca7
Christian Morante <christianmorante@outlook.com>
2021-05-26 13:37:12 -0500

    pruebas en 09-promesas2

--- 03-counter-app/src/tests/base/09-promesas.test.js
+++ 03-counter-app/src/tests/base/09-promesas.test.js
@@ -1,11 +1,14 @@
 import { getHeroeByIdAsync } from "../../base/09-promesas";
+import heroes from "../../data/heroes";
 
 describe('pruebas promesas', () => {
    
-    test('should return hero async', () => {
+    test('should return hero async', ( done ) => {
         const heroId = 1;
-        getHeroeByIdAsync( heroId ).then( (hero) => {
-            console.log(hero);
+        getHeroeByIdAsync( heroId ).then( hero => {
+            //console.log(hero);
+            expect( hero ).toEqual(heroes.find(h => h.id === heroId));
+            done();
         });
         
     })
