51c0493db58c699361af1ec22d7463d6f8c30a1f
Christian Morante <christianmorante@outlook.com>
2021-06-01 10:31:56 -0500

    Counter app finish

--- 05-hooks-app/src/HooksApp.js
+++ 05-hooks-app/src/HooksApp.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { CounterApp } from './components/01-useState/CounterApp'
 
 export const HooksApp = () => {
     return (
         <div>
             <h1>Hooks App</h1>
+            <hr />
+            <CounterApp />
         </div>
     )
 }
