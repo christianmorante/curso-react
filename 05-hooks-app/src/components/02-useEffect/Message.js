c9f84454eeeadb8752f67e92cb00fd7488478d61
Christian Morante <christianmorante@outlook.com>
2021-06-08 02:24:14 -0500

    modificaciones al home

--- 05-hooks-app/src/components/02-useEffect/Message.js
+++ 05-hooks-app/src/components/02-useEffect/Message.js
@@ -1,17 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({ x:0, y:0 })
+
+    const {x, y } = coords;
+
     useEffect(() => {
+        const mousemove = (e) => {
+            const coords = { x:e.x, y: e.y };
+            //console.log(coords);
+            setCoords(coords);
+            //console.log(':-)');
+        }
+        
+
+        window.addEventListener('mousemove', mousemove);
+        
         console.log('componente montado');
+
         return () => {
             console.log('componente desmontado');
+            window.removeEventListener('mousemove', mousemove);
         }
     },[])
 
     return (
         <div>
             <h3>Eres Genial!</h3>
+            <p>x: { x } y: { y }</p>
         </div>
     )
 }
